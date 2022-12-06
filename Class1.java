d4f1180d016e9ddb37b350cd31d8728963955c45
Hemant <hemant894@gmail.com>
2022-12-06 19:26:51 +0000

    class

---
+++ Class1.java
@@ -0,0 +1,7 @@
+package Revision12;
+
+public class Class1 {public static void main (String[]args){System.out.println(" Software Testing");
+System.out.println(" Manual testing and Automation Testing");
+System.out.println(" Group Revision");
+}
+}
